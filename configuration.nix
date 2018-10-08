617f992938dcfa0926528921bc76c2ba2003301f
Carl Hedgren <carl.hedgren@creuna.no>
2018-10-08 09:36:56 +0200

    chore: upgrade to 18.09

--- configuration.nix
+++ configuration.nix
@@ -156,6 +156,6 @@ services.xserver = {
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+  system.stateVersion = "18.09"; # Did you read the comment?
 
 }
